b5f442f1df8eda53203b46a400ecc7e968bcd58a
Kilo Loco <kiloloco@me.com>
2020-06-20 20:35:36 -0700

    add api

--- Cam-Gallery/Cam-Gallery/AppDelegate.swift
+++ Cam-Gallery/Cam-Gallery/AppDelegate.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2020 Kilo Loco. All rights reserved.
 //
 
-//import Amplify
-//import AmplifyPlugins
+import Amplify
+import AmplifyPlugins
 import UIKit
 
 @UIApplicationMain
@@ -15,24 +15,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-//        do {
-//            try Amplify.add(plugin:
-//                AWSAPIPlugin(modelRegistration: AmplifyModels())
-//            )
-//            try Amplify.add(plugin: AWSDataStorePlugin(
-//                modelRegistration: AmplifyModels())
-//            )
+        do {
+            let models = AmplifyModels()
+            try Amplify.add(
+                plugin: AWSDataStorePlugin(modelRegistration: models)
+            )
+            try Amplify.add(
+                plugin: AWSAPIPlugin(modelRegistration: models)
+            )
 //
 //            try Amplify.add(plugin: AWSCognitoAuthPlugin())
 //            try Amplify.add(plugin: AWSS3StoragePlugin())
-//
-//            try Amplify.configure()
-//
-//            print("Successfully initialized Amplify")
-//
-//        } catch {
-//            print("Could not initialize Amplify - \(error)")
-//        }
+
+            try Amplify.configure()
+
+            print("Successfully initialized Amplify")
+
+        } catch {
+            print("Could not initialize Amplify - \(error)")
+        }
         
         return true
     }
